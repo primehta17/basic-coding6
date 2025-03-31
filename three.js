86f30b3b2682495acb165929b416e2de86a90c85
Priyanka Mehta <primehta17@gmail.com>
2025-03-31 19:33:04 +0530

    square of circle

---
+++ three.js
@@ -0,0 +1,9 @@
+//area of circle
+
+let radius=2;
+
+let square= Math.PI*radius*radius;
+let sq=Math.PI*Math.pow(radius,2)
+console.log(sq);
+let area=square.toFixed(4);
+console.log(area);
---
+++ two.js
@@ -0,0 +1,8 @@
+//reverese the string
+
+let str="i like this program very much";
+let strjoin= '';
+for(let i=str.length-1;i>=0;i--){
+    strjoin+=str[i];
+}
+console.log(strjoin);
