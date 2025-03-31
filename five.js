95978605e544b04e99787c2ab95432b8ff0ba851
Priyanka Mehta <primehta17@gmail.com>
2025-03-31 19:55:28 +0530

    table of number

---
+++ five.js
@@ -0,0 +1,7 @@
+//multiplication table
+let num=2;
+
+for(let i=1;i<=10;i++){
+  let table=  num*i
+  console.log("table of "+num +" "+table)
+}
---
+++ four.js
@@ -0,0 +1,4 @@
+//generate random number till two digit
+
+let random= Math.floor(Math.random()*100);
+console.log(random);
